c03668ff1ff3392cf3277941c9d1d498c1293e4d
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2023-02-17 17:23:18 -0400

    [feat]: req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,23 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
+
+base_message = "banana"
+invalid_message= 1000
+r3_message= "nab_ana"
+r4_message= "an_anab"
+rN_message="ananab"
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(base_message, "b")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(invalid_message, 3)
+    # chamadas mal sucedidas
+    assert encrypt_message(base_message, 3) != r4_message
+    assert encrypt_message(base_message, 4) != rN_message
+    assert encrypt_message(base_message, 10) != r3_message
+    # chamadas bem sucedidas
+    assert encrypt_message(base_message, 3) == r3_message
+    assert encrypt_message(base_message, 4) == r4_message
+    assert encrypt_message(base_message, 10) == rN_message
