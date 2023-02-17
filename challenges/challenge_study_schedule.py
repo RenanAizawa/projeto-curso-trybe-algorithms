6ed45aeb729a33467e31baf3ac2f1d1609337c01
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2023-02-17 12:01:24 -0400

    [feat]: iniciando req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    count_target = 0
+    try: 
+        for dupla in permanence_period:
+            num1 = int(dupla[0])
+            num2 = int(dupla[1])
+            time = int(target_time)
+            if num1 == time or num2 == time:
+                count_target += 1
+    except Exception:
+        return None
+    return count_target
